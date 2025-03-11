5179106e19b75e74f55687251e2b89f326cb0aa5
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-03-11 10:36:40 +0100

    fix ros1 cmake export

--- cmake/brace_top_setup_brace_INSTALL_PREFIX.cmake
+++ cmake/brace_top_setup_brace_INSTALL_PREFIX.cmake
@@ -1,2 +1,4 @@
-set(brace_top_setup_brace_INSTALL_PREFIX "${brace_top_setup_brace_DIR}/../")
-get_filename_component(brace_top_setup_brace_INSTALL_PREFIX "${brace_top_setup_brace_INSTALL_PREFIX}" REALPATH)
+cmake_path(SET brace_top_setup_brace_INSTALL_PREFIX NORMALIZE "${brace_top_setup_brace_DIR}/../")
+get_filename_component(
+  brace_top_setup_brace_INSTALL_PREFIX "${brace_top_setup_brace_INSTALL_PREFIX}" REALPATH
+)
