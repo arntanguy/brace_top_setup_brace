273d7d9b886bed28870c959b6305c6765bfc5b7b
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-03-11 10:37:32 +0100

    Setup and run pre-commit

--- cmake/brace_top_setup_brace_INSTALL_PREFIX.cmake
+++ cmake/brace_top_setup_brace_INSTALL_PREFIX.cmake
@@ -1,4 +1,7 @@
-cmake_path(SET brace_top_setup_brace_INSTALL_PREFIX NORMALIZE "${brace_top_setup_brace_DIR}/../")
+cmake_path(
+  SET brace_top_setup_brace_INSTALL_PREFIX NORMALIZE "${brace_top_setup_brace_DIR}/../"
+)
 get_filename_component(
-  brace_top_setup_brace_INSTALL_PREFIX "${brace_top_setup_brace_INSTALL_PREFIX}" REALPATH
+  brace_top_setup_brace_INSTALL_PREFIX "${brace_top_setup_brace_INSTALL_PREFIX}"
+  REALPATH
 )
